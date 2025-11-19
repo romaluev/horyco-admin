103566a2ff928436e4c35254953170d9de70666d
magalutfullaev <mhoja9494@gmail.com>
2025-11-19 20:00:25 +0500

    fix: return purple color

--- src/features/product-form/ui/create-product-form.tsx
+++ src/features/product-form/ui/create-product-form.tsx
@@ -251,8 +251,8 @@ export const CreateProductForm = () => {
               disabled={expandLoading}
               className="relative inline-flex h-9 w-60 overflow-hidden rounded-lg p-[2px] focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 focus:outline-none"
             >
-              <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#023055_0%,#41B06E_50%,#023055_100%)]" />
-              <span className="bg-background inline-flex h-full w-full cursor-pointer items-center justify-center gap-2 rounded-md px-4 py-1 text-sm font-medium text-[#023055] backdrop-blur-3xl transition hover:text-[#41B06E]">
+              <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#023055_0%,#fe4a49_50%,#023055_100%)]" />
+              <span className="bg-background inline-flex h-full w-full cursor-pointer items-center justify-center gap-2 rounded-md px-4 py-1 text-sm font-medium text-[#023055] backdrop-blur-3xl transition hover:text-[#fe4a49]">
                 <StarsIcon size={16} />
                 {expandLoading ? 'Обрабатывается...' : 'Расширить с AI'}
               </span>
