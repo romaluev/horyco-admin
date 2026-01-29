845a541227f8f220d395bf03641eb31b71e835a4
Roma Lutfullaev <romalutfullaev@gmail.com>
2026-01-29 13:36:16 +0500

    lint fix

--- src/entities/menu/category/ui/category-tree.tsx
+++ src/entities/menu/category/ui/category-tree.tsx
@@ -7,15 +7,14 @@
 
 import { useState } from 'react'
 
-import type {
- DragEndEvent, DragStartEvent ,
+import {
   DndContext,
   DragOverlay,
   PointerSensor,
   useSensor,
   useSensors,
-  type UniqueIdentifier
-                              } from '@dnd-kit/core';
+} from '@dnd-kit/core'
+import type { DragEndEvent, DragStartEvent, UniqueIdentifier } from '@dnd-kit/core'
 import { SortableContext, verticalListSortingStrategy } from '@dnd-kit/sortable'
 
 import { CategoryTreeItem } from './category-tree-item'
